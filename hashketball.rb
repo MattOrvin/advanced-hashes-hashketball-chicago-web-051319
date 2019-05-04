df0be8c68f3cb5723264c61c78da84f4b2b555a2
Matt Orvin <matthew.orvin@gmail.com>
2019-05-04 19:13:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,13 +149,15 @@ def team_names
 end
 
 def player_numbers(team_name)
+  array = []
   game_hash.collect do |location, team_data|
     if team_data[:team_name].include?(team_name)
       team_data[:players].values.collect do |key, value|
-      key[:number]
+      array << key[:number]
      end
     end
   end
+  array
 end
 
 def player_stats(player_name)
